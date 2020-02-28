85cd8fce7f807b1dec9217a879b47e6edb7ed2c3
vchanyoung <dk90801@gmail.com>
2020-02-27 21:28:36 -0500

    upgrade: interim

--- A2/data/Assignment2.java
+++ A2/data/Assignment2.java
@@ -80,6 +80,7 @@ public class Assignment2 {
     */
    public boolean bookSeat(int passID, int flightID, String seatClass) {
       // Implement this method!
+
       return false;
    }
 
@@ -99,6 +100,21 @@ public class Assignment2 {
     */
    public int upgrade(int flightID) {
       // Implement this method!
+      try 
+      {
+        PreparedStatement ps;
+        ResultSet rs;
+        String allseats = "select * from flight where flightID =" + flightID;
+  
+        ps = connection.prepareStatement(allseats);
+  
+        rs = ps.executeQuery();
+      }
+      catch (SQLException e)
+      {
+        System.err.println("SQL Exception." + e.getMessage());
+      }
+      // if empty seat, return 1 
       return -1;
    }
 
