5f4341b3e70c84560e450646990d8418141cfbd2
vchanyoung <dk90801@gmail.com>
2020-02-28 20:25:29 -0500

    update test

--- A2/data/Assignment2.java
+++ A2/data/Assignment2.java
@@ -154,7 +154,7 @@ public class Assignment2 {
         ps.setInt(3, flightID);
         //ps.setInt(4, price);
         //ps.setInt(5, price);
-        
+
       } catch (SQLException se){
         System.err.println("SQL Exception." + se.getMessage());
         return false;
@@ -184,13 +184,16 @@ public class Assignment2 {
 
         String allflights = "select * from flight where flightID =" + flightID;
         // java.sql.Timestamp time = getCurrentTimeStamp();
-
         ps = connection.prepareStatement(allflights);
-
         rs = ps.executeQuery();
         // check if the flight is empty
         if (rs.getInt("count") == 0) {
           System.out.println("The flight does not exist.");
+          return -1;
+        }
+        // check flight exists
+        if (!checkFlightExistsAndNotDeparted(flightID)){
+          System.out.println("The flight either does not exist or alreadt has been departed");
           return -1;
         }
         else {
@@ -198,11 +201,7 @@ public class Assignment2 {
           // seat is booked
           String allseats = "select * from plane where flightID =" + flightID;
           rs = ps.executeQuery();
-          // if flight departed, then print "no seats available", return -1
-          // else, execute below condition
-
-
-
+          
           // check seat capacity
           ps = connection.prepareStatement(allseats);
           rs = ps.executeQuery();
