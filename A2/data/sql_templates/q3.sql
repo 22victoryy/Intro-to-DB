7ca80e81dde8756a864ff19d771667cafba4b64a
Ahmad Al Saidi <ahmad.alsaidi@mail.utoronto.ca>
2020-03-03 20:22:35 -0500

    Update q3.sql

--- A2/data/sql_templates/q3.sql
+++ A2/data/sql_templates/q3.sql
@@ -16,11 +16,36 @@ CREATE TABLE q3 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS OnApril302020 CASCADE;
 
 
 -- Define views for your intermediate steps here:
 
+CREATE VIEW OnApril302020 AS
+SELECT id, outbound, inbound, s_dep, s_arv
+FROM Flight
+WHERE date_part('day', s_dep)   = 30   AND
+      date_part('month', s_dep) = 4    AND
+      date_part('year', s_dep)  = 2020 AND
+      date_part('day', s_arv)   = 30   AND
+      date_part('month', s_arv) = 4    AND
+      date_part('year', s_arv)  = 2020;
 
+CREATE VIEW CanadaToUSA AS
+SELECT a1.code AS outbound_code, a2.code AS inbound_code 
+FROM Airport a1, Airport a2
+WHERE a1.country='Canada' AND a2.country='USA';
+
+CREATE VIEW AllPairs AS
+(SELECT * FROM CanadaToUSA)
+UNION
+(SELECT inbound_code AS outbound_code, outbound_code AS inbound_code
+ FROM CanadaToUSA);
+
+CREATE VIEW Direct AS
+SELECT * 
+FROM AllPairs JOIN OnApril302020
+     ON outbound_code=outbound and inbound_code=inbound;
+     
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q3
