4e525754d3efaa5a22e1d42c25eb0f19a02ab5d9
vchanyoung <dk90801@gmail.com>
2020-03-03 19:36:47 -0500

    q2: test

--- A2/data/sql_templates/q2.sql
+++ A2/data/sql_templates/q2.sql
@@ -56,7 +56,7 @@ WHERE (kind='international' and dep_interval > '10:00:00') or
 
 CREATE VIEW total AS
 SELECT SUM(booking.price)
-FROM 35percenters UNION 50percenters INNER JOIN booking
+FROM thirtyfivepercenters UNION fiftypercenters INNER JOIN booking
 
 INSERT INTO q2
 
