eb03c459e21cfe45dbdff162ba5f4b03ef4beeb6
Ahmad Al Saidi <ahmad.alsaidi@mail.utoronto.ca>
2020-03-21 19:09:11 -0400

    Update q2.sql

--- A3/submission/q2.sql
+++ A3/submission/q2.sql
@@ -2,6 +2,7 @@ SET SEARCH_PATH TO wetworldschema, public;
 
 DROP VIEW IF EXISTS AvgSiteRating CASCADE;
 DROP VIEW IF EXISTS AvgMonitorRating CASCADE;
+DROP VIEW IF EXISTS BetterMonitors CASCADE;
 
 CREATE VIEW AvgSiteRating AS
 SELECT MonitorAffiliations.site_id AS site_id,
@@ -19,9 +20,20 @@ WHERE MonitorRating.id = Booking.id AND
       Booking.affiliation_id = MonitorAffiliations.id
 GROUP BY MonitorAffiliations.monitor_id;
 
+CREATE VIEW BetterMonitors AS
 SELECT monitor_id
 FROM AvgMonitorRating
 WHERE avg_rating > ALL (SELECT avg_rating
                         FROM AvgSiteRating, MonitorAffiliations MA
                         WHERE AvgSiteRating.site_id = MA.site_id
                         AND MA.monitor_id = AvgMonitorRating.monitor_id);
+
+CREATE VIEW BetterMonitorsAvgFee AS
+SELECT BetterMonitors.monitor_id, avg(MonitorAffiliations.price) AS avg_fees
+FROM BetterMonitors, MonitorAffiliations
+WHERE BetterMonitors.monitor_id = MonitorAffiliations.monitor_id
+GROUP BY BetterMonitors.monitor_id;
+
+SELECT email, avg_fees
+FROM Monitor, BetterMonitorsAvgFee
+WHERE Monitor.id = BetterMonitorsAvgFee.monitor_id;
