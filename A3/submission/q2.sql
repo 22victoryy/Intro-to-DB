dc7f5a2d5777cb1d87e798f47d4f8633959f9242
Ahmad Al Saidi <ahmad.alsaidi@mail.utoronto.ca>
2020-03-21 21:48:00 -0400

    comments

--- A3/submission/q2.sql
+++ A3/submission/q2.sql
@@ -5,7 +5,7 @@ DROP VIEW IF EXISTS AvgMonitorRating CASCADE;
 DROP VIEW IF EXISTS BetterMonitors CASCADE;
 DROP VIEW IF EXISTS BetterMonitorsAvgFee CASCADE;
 
-
+-- For every site that has been rated the view contains its average rating
 CREATE VIEW AvgSiteRating AS
 SELECT MonitorAffiliations.site_id AS site_id,
        avg(SiteRating.rating) AS avg_rating 
@@ -14,6 +14,7 @@ WHERE SiteRating.booking_id = Booking.id AND
       Booking.affiliation_id = MonitorAffiliations.id
 GROUP BY MonitorAffiliations.site_id;
 
+-- For every monitor that has been rated the view contains their average rating
 CREATE VIEW AvgMonitorRating AS
 SELECT MonitorAffiliations.monitor_id AS monitor_id,
        avg(MonitorRating.rating) AS avg_rating 
@@ -22,6 +23,8 @@ WHERE MonitorRating.id = Booking.id AND
       Booking.affiliation_id = MonitorAffiliations.id
 GROUP BY MonitorAffiliations.monitor_id;
 
+-- The view contains the monitor ids of monitors who have a higher average
+-- rating than all of the sites they monitor in
 CREATE VIEW BetterMonitors AS
 SELECT monitor_id
 FROM AvgMonitorRating
@@ -30,12 +33,16 @@ WHERE avg_rating > ALL (SELECT avg_rating
                         WHERE AvgSiteRating.site_id = MA.site_id
                         AND MA.monitor_id = AvgMonitorRating.monitor_id);
 
+-- The view contains the monitor id and their average fee for monitors who have
+-- a higher average rating than all of the sites they monitor in
 CREATE VIEW BetterMonitorsAvgFee AS
 SELECT BetterMonitors.monitor_id, avg(MonitorAffiliations.price) AS avg_fees
 FROM BetterMonitors, MonitorAffiliations
 WHERE BetterMonitors.monitor_id = MonitorAffiliations.monitor_id
 GROUP BY BetterMonitors.monitor_id;
 
+-- The query shows the email and avg fees for  monitors who have a higher
+-- average rating than all of the sites they monitor in
 SELECT email, avg_fees
 FROM Monitor, BetterMonitorsAvgFee
 WHERE Monitor.id = BetterMonitorsAvgFee.monitor_id;
