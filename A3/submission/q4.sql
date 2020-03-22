a0077eb9799ce67e1acc04e269d45d1aa53d195f
vchanyoung <dk90801@gmail.com>
2020-03-21 20:33:48 -0400

    q4 update

--- A3/submission/q4.sql
+++ A3/submission/q4.sql
@@ -1,40 +1,10 @@
 SET SEARCH_PATH TO wetworldschema, public;
 
+DROP VIEW IF EXISTS ALLFEES CASCADE;
+
 CREATE VIEW ALLFEES AS
 SELECT price
-FROM subbooking INNER JOIN MonitorAffiliations ON (MonitorAffiliations.monitor_id = subboking.diver_id)
-;
+FROM subbooking INNER JOIN MonitorAffiliations ON (MonitorAffiliations.monitor_id = subbooking.diver_id);
 
-
-CREATE TABLE MonitorAffiliations (
-id INT PRIMARY KEY,
--- monitor id
-monitor_id INT REFERENCES Monitor,
--- the dive type
-dive_type dive_type NOT NULL,
--- the affiliated site id
-site_id INT REFERENCES DiveSite,
--- the dive time of the diving
-dive_time dive_time NOT NULL,
--- price for monitor site type combination
-price NUMERIC NOT NULL CHECK (price > 0),
-UNIQUE (monitor_id, site_id, dive_time, dive_type)
-);
-
-
-CREATE TABLE SubBooking (
--- the booking the diver is included in
-booking_id INT REFERENCES Booking,
--- the diver associated with the booking
-diver_id INT REFERENCES Diver,
--- extras
--- fee paid for mask, zero if not purchased
-mask NUMERIC NOT NULL CHECK (mask >= 0) DEFAULT 0,
--- fee paid for regulator, zero if not purchased
-regulator NUMERIC NOT NULL CHECK (regulator >= 0) DEFAULT 0,
--- fee paid for fins, zero if not purchased
-fins NUMERIC CHECK (fins >= 0) DEFAULT 0,
--- fee paid for wrist-mounted dive computer, zero if not purchased
-computer NUMERIC CHECK (computer >= 0) DEFAULT 0,
-PRIMARY KEY (booking_id, diver_id)
-);
+SELECT avg(price), min(price), max(price)
+FROM ALLFEES;
