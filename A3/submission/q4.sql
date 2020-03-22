cf25c50f6faf439ed91389b3e46c7adb869b0a80
Ahmad Al Saidi <ahmad.alsaidi@mail.utoronto.ca>
2020-03-21 21:42:50 -0400

    comments

--- A3/submission/q4.sql
+++ A3/submission/q4.sql
@@ -2,12 +2,18 @@ SET SEARCH_PATH TO wetworldschema, public;
 
 DROP VIEW IF EXISTS ALLFEES CASCADE;
 
+-- The view includes for every booking the total fees paid for the booking
+-- excluding extra fees 
 CREATE VIEW ALLFEES AS
-SELECT sum(Booking.price), site_id
+SELECT sum(Booking.price) AS total_fees, site_id
 FROM SubBooking, MonitorAffiliations, Booking
-WHERE Booking.affiliation_id = MonitorAffiliations.id AND SubBooking.booking_id = Booking.id
+WHERE Booking.affiliation_id = MonitorAffiliations.id
+      AND SubBooking.booking_id = Booking.id
 GROUP BY booking_id,site_id;
 
-SELECT site_id, avg(sum), min(sum), max(sum)
+-- The query will get for every site its average, minimum and maximum fees paid
+-- for any booking that takes place in it. If a site did not get any bookings
+-- then it is not included in this query
+SELECT site_id, avg(total_fees), min(total_fees), max(total_fees)
 FROM ALLFEES
 GROUP BY ALLFEES.site_id;
