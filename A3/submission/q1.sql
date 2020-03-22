6d8c81e5cad2d27520ad20bdef7f64ef0a024f8d
Ahmad Al Saidi <ahmad.alsaidi@mail.utoronto.ca>
2020-03-21 22:02:42 -0400

    Update q1.sql

--- A3/submission/q1.sql
+++ A3/submission/q1.sql
@@ -1,5 +1,12 @@
 SET SEARCH_PATH TO wetworldschema, public;
 
-SELECT dive_type, count(distinct site_id)
-FROM MonitorAffiliations
-GROUP BY dive_type;
+DROP VIEW IF EXISTS DiveTypes CASCADE;
+
+-- creates a view with all dive types in one column
+CREATE VIEW DiveTypes (dive_type) AS VALUES
+('open water'), ('cave'), ('deeper than 30');
+
+SELECT DiveTypes.dive_type, count(distinct site_id)
+FROM DiveTypes LEFT JOIN MonitorAffiliations
+     ON CAST (DiveTypes.dive_type AS dive_type) = MonitorAffiliations.dive_type
+GROUP BY DiveTypes.dive_type;
